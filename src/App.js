f81a289b131421d5a3874236e2452c2dfe354d63
EmanuelPRA <emanuel.pracic@gmail.com>
2022-08-20 13:26:23 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const BMI = () =>{
+  const [height, setHeight] = useState(1.70)
+  const [weight, setWeight] = useState(65)
+  const [submitted, setSubmitted] = useState(false)
+  let bmindex = weight / (height) ** 2
+  return(
+    <>
+    {!submitted &&(
+      <>
+      <input type="text" placeholder='Height m' onChange={(e) => setHeight(e.target.value)}/>
+      <input type="text" placeholder='Weight kg' onChange={(e) => setWeight(e.target.value)}/>
+      <button onClick={() => setSubmitted(true)}>Submit</button>
+      </>
+      )}
+      {submitted &&(
+      <>
+      <h2>Your BMI is {bmindex}</h2>
+      <button onClick={() => setSubmitted(false)}>Edit</button>
+      </>
+      )}
+    </>
+
+  )
+}
+
+const CalorieCalc = () =>{
+  return(
+    <>
+      <input type="text" placeholder='Height cm'/>
+      <input type="text" placeholder='Weight kg'/>
+      <input type="radio" value="male" id="male"></input>
+      <label for="male">Male</label>
+      <input type="radio" value="female" id="female"></input>
+      <label for="female">Female</label>
+      <select id="cars" name="cars">
+        <option value="sedentary">Sedentary(No exercise)</option>
+        <option value="light">Lightly active(1-3 times per week)</option>
+        <option value="moderate">Moderatly active(3-5 times per week)</option>
+        <option value="very">Very Active(6-7 days per week)</option>
+        <option value="extreme">Extremely Active(hard labour job)</option>
+    </select>
+    </>
+
+  )
+}
+
+
 
 function App() {
+
+  const [calculator, setCalculator] = useState("BMI")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {calculator === "BMI" &&(<BMI/>)}
+    {calculator === "CC" &&(<CalorieCalc/>)}
+    
+
+    <ul>
+      <li><button onClick={() =>{setCalculator("BMI")}}>BMI Calculator</button></li>
+      <li><button onClick={() =>{setCalculator("CC")}}>Calorie Calculator</button></li>
+
+
+    </ul>
+    
+    </>
+    
   );
 }
 
